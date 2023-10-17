1742cfe9edecec67460b8fd9e280877fef752bb0
Eniola <morakinyoelisha01@gmail.com>
2023-10-17 16:44:22 -0700

    octal file added

--- main.h
+++ main.h
@@ -25,6 +25,7 @@ int print_char(va_list c);
 int print_int(va_list i);
 int print_dec(va_list d);
 int print_bin(va_list b);
+int print_oct(va_list o);
 int print_rev(va_list r);
 int print_octal(va_list o);
 int print_x(va_list x);
