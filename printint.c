c040ed31d2a65747220f15228550dce2bbdaa67e
Henzz <khenok53@gmail.com>
2023-10-18 22:16:37 +0300

    resolved betty style for printint.c

--- printint.c
+++ printint.c
@@ -1,36 +1,36 @@
-#include "main.h"
-
 /**
- * printInteger: prints integers up to 20 digits
- * @num: number to print
+ * printInteger - prints an integer up to 20 digits
+ * @num: the number to print
 */
-void printInteger(int num) {
-    /* Allocate a buffer to store the digits (assuming a maximum of 20 digits) */
-    char digits[20];
-    char digit = '0', digitChar;
-    int index = 0, digitValue, i;
+void printInteger(int num)
+{
+	/* Allocate a buffer to store the digits (assuming a maximum of 20 digits) */
+	char digits[20];
+	char digit = '0', digitChar;
+	int index = 0, digitValue, i;
 
-    if (num == 0) {
-        digit = '0';
-        _putchar(digit);
-        return;
-    }
+	if (num == 0)
+	{
+		digit = '0';
+		_putchar(digit);
+		return;
+	}
 
-    if (num < 0) {
-        _putchar('-');
-        num = -num;
-    }
+	if (num < 0)
+	{
+		_putchar('-');
+		num = -num;
+	}
 
-    while (num > 0) {
-        digitValue = num % 10;
-        digitChar = '0' + digitValue;
-        digits[index++] = digitChar;
-        num /= 10;
-    }
+	while (num > 0)
+	{
+		digitValue = num % 10;
+		digitChar = '0' + digitValue;
+		digits[index++] = digitChar;
+		num /= 10;
+	}
 
-    /* Print the digits in reverse order */
-    for (i = index - 1; i >= 0; i--)
-    {
-        _putchar(digits[i]);
-    }
+	/* Print the digits in reverse order */
+	for (i = index - 1; i >= 0; i--)
+		_putchar(digits[i]);
 }
