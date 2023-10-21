c8345bbb8eb5d636f92fa382bd4766a466884eea
Eniola <morakinyoelisha01@gmail.com>
2023-10-20 19:20:46 -0700

    fixes

--- print_ptr.c
+++ print_ptr.c
@@ -8,17 +8,38 @@
  */
 int print_ptr(va_list p)
 {
-	int count = 0;
+	int i, count = 0;
+	const char null_str[] = "(null)";
 	unsigned long num;
 	void *ptr = va_arg(p, void *);
+	char hex_buffer[32];
+	char hex_chars[] = "0123456789abcdef";
+
+	if (ptr == NULL)
+	{
+		for (i = 0; null_str[i] != '\0'; i++)
+		{
+			_putchar(null_str[i]);
+		}
+		return (6);
+	}
+	num = (unsigned long)ptr;
 
 	_putchar('0');
 	_putchar('x');
 	count += 2;
 
 	/* convert and print the pointer in hexadecimal. */
-	num = (unsigned long)ptr;
-	count += print_decimalToHexCode(num);
+	while (num > 0)
+	{
+		hex_buffer[count] = hex_chars[num % 16];
+		count++;
+		num /= 16;
+	}
+	for (i = count - 1; i >= 2; i--)
+	{
+		_putchar(hex_buffer[i]);
+	}
 
 	return (count);
 }
