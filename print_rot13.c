52865ef993622fdbbf5af7db5218563b71b56827
Eniola <morakinyoelisha01@gmail.com>
2023-10-20 18:44:21 -0700

    Optimization

--- print_rot13.c
+++ print_rot13.c
@@ -1,29 +1,35 @@
 #include "main.h"
+
 /**
- **print_Rot13- function that encodes a string using rot13
- *@R: input
- * Return: void
+ * print_rot13 - Print the ROT13'd version of a string
+ * @R: Argument from va_list
+ *
+ * Return: The number of characters printed
  */
 int print_rot13(va_list R)
-{	int i, j;
+{
+	int i, j;
 	int sum = 0;
 	char *s = va_arg(R, char *);
-char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; a[j] != '\0'; j++)
-		if (s[i] == a[j])
-	{
-		_putchar(b[j]);
-		sum++;
-		break;
-	}
-	if (!a[j])
-	{
-		_putchar(s[i]);
-		sum++;
-	}
+		{
+			if (s[i] == a[j])
+			{
+				_putchar(b[j]);
+				sum++;
+				break;
+			}
+		}
+		if (!a[j])
+		{
+			_putchar(s[i]);
+			sum++;
+		}
 	}
 	return (sum);
 }
